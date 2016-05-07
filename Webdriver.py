a35382f08710b0047cdc6531b6497907343b3983
GN3 <galnachmana@gmail.com>
2016-05-07 18:50:30 +0300

    video added, need to fix allignment

--- Webdriver.py
+++ Webdriver.py
@@ -7,8 +7,8 @@ class Webdriver:
 
     def __init__(self):
         # self.browser = webdriver.Chrome()  # Create Chrome browser
-        # self.browser = webdriver.Chrome(executable_path="/Users/GN3/Desktop/chromedriver")  # Create Chrome browser
-        self.browser = webdriver.Chrome("env/chromedriver")  # Create Chrome browser
+        self.browser = webdriver.Chrome(executable_path="/Users/GN3/Desktop/chromedriver")  # Create Chrome browser
+        # self.browser = webdriver.Chrome("env/chromedriver")  # Create Chrome browser
         self.browser.maximize_window()
 
     def close(self):
