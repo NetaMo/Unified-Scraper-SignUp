f6106043ed47ee875376eaa454769aa558d95774
Ayal <ayal.moses@gmail.com>
2016-05-08 20:59:52 +0300

    cleaned prints

--- WhatsAppDB.py
+++ WhatsAppDB.py
@@ -260,20 +260,6 @@ class WhatsAppDB:
         past_chats_threshold_date = min(self.contacts_df.time) + to_offset(past_chats_threshold_days)
         # print(past_chats_threshold_date)
         df_past_chats = self.contacts_df.where(self.contacts_df.time <= past_chats_threshold_date).dropna()
-        # res_df = pd.DataFrame()
-        # stop_flag = False
-        # FROM = 0
-        # TO = 1
-        # TXT = 2
-        # for msg in df_past_chats.get_values():
-        #     print("from: {0:20}\tto: {1:20}\ttext: {2}".format(msg[FROM], msg[TO], msg[TXT]))
-        #     # print(type(msg))
-        #     # if fdsafds:
-        #     #     res_df.
-        #     # if not ..:
-        #     #     break
-        # # while not stop_flag:
-
         return df_past_chats[['contactName', 'text']]  # todo check how to filter good past msgs
     
     def get_dreams_or_old_messages(self, past_fraction_param):
